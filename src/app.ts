c9fa9affe8119669af77a8a1b4bd3370dad350f0
Roman Maiun <roman.maiun@unicorn.com>
2023-05-03 18:37:14 +0300

    env variables check

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import path from "path";
 import CatRepo from "./repos/CatRepo";
+import * as process from "process";
 // initialize configuration
 dotenv.config();
 
@@ -10,15 +11,22 @@ dotenv.config();
 const port = process.env.SERVER_PORT;
 
 const app = express();
-
+console.log(`${JSON.stringify(process.env)}`)
 // Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
+// app.set( "views", path.join( __dirname, "views" ) );
+// app.set( "view engine", "ejs" );
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
+
+app.get( "/", async ( req, res ) => {
     // render the index template
-    res.render( "index" );
+    res.json( {rootGet: "OK"} );
+} );
+app.post( "/", async ( req, res ) => {
+    // render the index template
+    const cat = req.body
+    await CatRepo.createCat(cat);
+    res.json( {rootPost: "OK"} );
 } );
 
 // start the express server
